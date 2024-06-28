d0a1f8a01e5e7b00df23982c738000dbc08ff2fa
naterex <nathanieljrex@gmail.com>
2024-06-28 19:02:27 -0400

    Character

---
+++ Source/Slash/Private/Characters/SlashCharacter.cpp
@@ -0,0 +1,108 @@
+// Copyright (c) 2024 Nathaniel Rex. No Rights Reserved.
+
+
+#include "Characters/SlashCharacter.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+#include "GroomComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+
+ASlashCharacter::ASlashCharacter()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationRoll = false;
+
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	GetCharacterMovement()->RotationRate = FRotator(0.f, 400.f, 0.f);
+
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("SlashCameraBoom"));
+	CameraBoom->SetupAttachment(GetRootComponent());
+	CameraBoom->TargetArmLength = 300.f;
+
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("SlashCamera"));
+	Camera->SetupAttachment(CameraBoom);
+
+	USkeletalMeshComponent* CharacterMesh = GetMesh();
+
+	Hair = CreateDefaultSubobject<UGroomComponent>(TEXT("SlashHair"));
+	Hair->SetupAttachment(CharacterMesh);
+	Hair->AttachmentName = FString("head");
+
+	Eyebrows = CreateDefaultSubobject<UGroomComponent>(TEXT("SlashEyebrows"));
+	Eyebrows->SetupAttachment(CharacterMesh);
+	Eyebrows->AttachmentName = FString("head");
+}
+
+void ASlashCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ASlashCharacter::Jump()
+{
+	Super::Jump();
+}
+
+void ASlashCharacter::Look(const FInputActionValue& Value)
+{
+	const FVector2d LookInput = Value.Get<FVector2d>();
+	AddControllerYawInput(LookInput.X);
+	UE_LOG(LogTemp, Warning, TEXT("Y: %f"), LookInput.Y);
+	AddControllerPitchInput(LookInput.Y);
+}
+
+void ASlashCharacter::Move(const FInputActionValue& Value)
+{
+	const FVector2d MovementInput = Value.Get<FVector2d>();
+
+	const FRotator Rotation = GetControlRotation();
+	const FRotator YawRotation(0.f, Rotation.Yaw, 0.f);
+	const FRotationMatrix Matrix(YawRotation);
+
+	const FVector Forward = Matrix.GetUnitAxis(EAxis::X);
+	AddMovementInput(Forward, MovementInput.Y);
+
+	const FVector Right = Matrix.GetUnitAxis(EAxis::Y);
+	AddMovementInput(Right, MovementInput.X);
+}
+
+void ASlashCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ASlashCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	
+	// Set up input mapping context
+	if (APlayerController* PlayerController = Cast<APlayerController>(GetController()))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			Subsystem->AddMappingContext(InputMappingContext, 0);
+
+		}
+	}
+
+	// Bind input actions
+	if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		// Jumping
+		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Triggered, this, &ASlashCharacter::Jump);
+
+		// Looking
+		EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &ASlashCharacter::Look);
+
+		// Moving
+		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ASlashCharacter::Move);
+	}
+}
+
---
+++ Source/Slash/Public/Characters/SlashCharacter.h
@@ -0,0 +1,60 @@
+// Copyright (c) 2024 Nathaniel Rex. No Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "InputActionValue.h"
+#include "SlashCharacter.generated.h"
+
+class USpringArmComponent;
+class UCameraComponent;
+class UGroomComponent;
+class UInputMappingContext;
+class UInputAction;
+
+UCLASS()
+class SLASH_API ASlashCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	
+	ASlashCharacter();
+	virtual void Tick(float DeltaTime) override;
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+
+protected:
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
+	UInputMappingContext* InputMappingContext;
+
+	UPROPERTY(EditAnywhere, Category = Input)
+	UInputAction* JumpAction;
+
+	UPROPERTY(EditAnywhere, Category = Input)
+	UInputAction* LookAction;
+
+	UPROPERTY(EditAnywhere, Category = Input)
+	UInputAction* MoveAction;
+
+	virtual void BeginPlay() override;
+	virtual void Jump();
+	void Look(const FInputActionValue& Value);
+	void Move(const FInputActionValue& Value);
+
+private:
+
+	UPROPERTY(VisibleAnywhere)
+	USpringArmComponent* CameraBoom;
+
+	UPROPERTY(VisibleAnywhere)
+	UCameraComponent* Camera;
+
+	UPROPERTY(VisibleAnywhere, Category = Hair)
+	UGroomComponent* Hair;
+
+	UPROPERTY(VisibleAnywhere, Category = Hair)
+	UGroomComponent* Eyebrows;
+};
