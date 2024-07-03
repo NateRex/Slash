312c941962aaffc77942091f4434ac1e9f5cb3be
naterex <nathanieljrex@gmail.com>
2024-07-02 21:46:00 -0400

    Breakable actors

---
+++ Source/Slash/Public/Breakables/BreakableActor.h
@@ -0,0 +1,31 @@
+// Copyright (c) 2024 Nathaniel Rex. No Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Interfaces/HitInterface.h"
+#include "BreakableActor.generated.h"
+
+class UGeometryCollectionComponent;
+
+UCLASS()
+class SLASH_API ABreakableActor : public AActor, public IHitInterface
+{
+	GENERATED_BODY()
+	
+public:	
+	ABreakableActor();
+	virtual void Tick(float DeltaTime) override;
+	virtual void GetHit(const FVector& ImpactPoint) override;
+
+
+protected:
+	virtual void BeginPlay() override;
+	
+
+private:
+
+	UPROPERTY(VisibleAnywhere)
+	UGeometryCollectionComponent* GeometryCollection;
+};
